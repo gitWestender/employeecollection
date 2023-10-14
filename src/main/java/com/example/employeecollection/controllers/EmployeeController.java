62ff5f0f8cf512fe503990cbbad6adbc7a59476c
Westender <javaxwestender@gmail.com>
2023-10-14 16:46:14 +0300

    Complete

--- src/main/java/com/example/employeecollection/controllers/EmployeeController.java
+++ src/main/java/com/example/employeecollection/controllers/EmployeeController.java
@@ -1,6 +1,6 @@
 package com.example.employeecollection.controllers;
 
-import com.example.employeecollection.services.IEmployeeService;
+import com.example.employeecollection.interfaces.IEmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
--- src/main/java/com/example/employeecollection/services/IEmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.example.employeecollection.services;
-
-public interface IEmployeeService {
-    String welcome();
-    String addEmployee(String firstName, String lastName);
-    String removeEmployee(String firstName, String lastName);
-    String findEmployee(String firstName, String lastName);
-}
