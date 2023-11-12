3c3d4f109bf339eb9650ab62d53f88ef02560a49
Westender <javaxwestender@gmail.com>
2023-11-12 21:41:40 +0300

    Complete

--- src/main/java/com/example/employeecollection/services/EmployeeService.java
+++ src/main/java/com/example/employeecollection/services/EmployeeService.java
@@ -5,6 +5,7 @@ import com.example.employeecollection.exeptions.EmployeeNotFoundException;
 import com.example.employeecollection.exeptions.EmployeeStorageIsFullException;
 import com.example.employeecollection.interfaces.IEmployeeService;
 import com.example.employeecollection.model.Employee;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -42,13 +43,20 @@ public class EmployeeService implements IEmployeeService {
         if (employeesList.size() > 10) {
             throw new EmployeeStorageIsFullException("Превышен лимит количества сотрудников в фирме");
         }
-        for (int i = 0; i < employeesList.size(); i++) {
-            if (employeesList.get(i).equals(temp)) {
-                throw new EmployeeAlreadyAddedException("Такой сотрудник уже присутствует");
+        if (StringUtils.isAlpha(firstName) && StringUtils.isAlpha(lastName)) {
+            for (int i = 0; i < employeesList.size(); i++) {
+                if (employeesList.get(i).equals(temp)) {
+                    throw new EmployeeAlreadyAddedException("Такой сотрудник уже присутствует");
+                }
             }
+            temp.setFirstName(StringUtils.capitalize(firstName));
+            temp.setLastName(StringUtils.capitalize(lastName));
+            employeesList.add(temp);
+            return "Сотрудник добавлен";
+        } else {
+            throw new RuntimeException();
         }
-        employeesList.add(temp);
-        return "Сотрудник добавлен";
+
     }
 
     public String removeEmployee(String firstName, String lastName, int id) {
